fb6f4b35cc931779731e41023a1de11b2b4ac3fd
Namiki-M <m.namiki.carecon@gmail.com>
2021-08-28 18:28:40 +0900

    資料作成

---
+++ python_lesson/script.py
@@ -0,0 +1,79 @@
+# import tkinter
+# root = tkinter.Tk()
+# root.geometry("400x240")
+
+# var = tkinter.IntVar()
+# var.set(0)
+
+# rdo1 = tkinter.Radiobutton(root,value=0
+# ,variable=var,text='ストアカ' ).pack() 
+
+# rdo2=tkinter.Radiobutton(root,value=1
+# ,variable=var,text='Python' ).pack()
+
+# rdo3 = tkinter.Radiobutton(root,value=2
+# ,variable=var,text='学習' ).pack()
+
+# var.get()
+
+
+# root.mainloop()
+
+# x = "ストアカ"
+# y = "楽しい!!"
+# print(x + y)  
+
+# print(len(y))
+
+# from tkinter import messagebox
+# import tkinter as tk
+# from tkinter import messagebox
+# import tkinter.simpledialog as simpledialog
+# root = tk.Tk()
+# root.withdraw() #小さなウィンドウを表示させない
+# messagebox.showinfo('お題目', 'ストアカ')
+# messagebox.showinfo('確認', 'もうすぐ誕生日です')
+
+# res = messagebox.showinfo("title", "message")
+# print(res)s
+
+# res = messagebox.askquestion("title", "今日は")
+# print("showwarning", res)
+
+# res = messagebox.askokcancel("title", "apex")
+# print("showwarning", res)
+
+# res = messagebox.askretrycancel("title", "できるの？")
+# print("showwarning", res)
+
+
+
+# from collections import Counter
+# # x = ['フ','ィ','ン','セ','ン','ト']
+# # print(Counter(x))
+# # print(1,2,3,4,4,5,sep='と')  
+
+# x = input('入力してね')
+# print(x.isdigit()) 
+
+# x = 2 
+# if x == 2 and x == 5:
+# 	print('あたり')
+# elif x == 3:
+#     print('どちらともいえない')
+# else : 
+#  	print('はずれ')
+
+# for x in range(0,11):
+# 	print(x,'日')
+# l = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+# for c in l[0:5]:
+#     print(c)
+
+x = ['野球','サッカー','バレー','カバディ']
+for y in x[1:3]:
+	print(y)
+
+
+
+
